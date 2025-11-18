bf995813a968d653719b1621290fdd7c534c0c51
Imnotshashwat <stdeternal@gmail.com>
2025-11-18 21:34:24 +0530

    Add hollow rectangle, star, and star with hash pattern programs

---
+++ PatternPrinting/hollowRectangle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    // int n, m;
+    // printf("enter the no of rows: ");
+    // scanf("%d", &n);
+    // printf("enter the no column: ");
+    // scanf("%d", &m);
+
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= 6; j++)
+        {
+            if ((j == 1 || j == 6) || (i == 1 || i == 4))  // hollow rectangle condition
+            {
+                printf("# ");
+            }
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
